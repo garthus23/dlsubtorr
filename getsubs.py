d1d1e85383f07220a29e3c9a2c9cb874a94ed0bf
Gregory ARTHUS <gr.arthus@gmail.com>
2023-01-24 23:07:00 +0000

    manage request error because webpage too heavy

--- getsubs.py
+++ getsubs.py
@@ -10,56 +10,57 @@ from torrentdl import torrentdl
 
 if __name__ == "__main__":
 
-	html_dl("https://www.addic7ed.com", "tvlist")
+        html_dl("https://www.addic7ed.com", "tvlist")
 
-	if len(sys.argv) == 4 :
-		name = sys.argv[1]
-		season = sys.argv[2]
-		episode = sys.argv[3]
-	else:
-		name = input("Show Title : ").lower()
-		season = input("Season : ")
-		episode = input("Episode : ")
-	
-	showlist = my_dictionary()
-	get_tvshow_list(showlist, name)
-	print_showlist(showlist, name)
-	name = input_show_choice(showlist)	
+        if len(sys.argv) == 4 :
+                name = sys.argv[1]
+                season = sys.argv[2]
+                episode = sys.argv[3]
+        else:
+                name = input("Show Title : ").lower()
+                season = input("Season : ")
+                episode = input("Episode : ")
+        
+        showlist = my_dictionary()
+        get_tvshow_list(showlist, name)
+        print_showlist(showlist, name)
+        name = input_show_choice(showlist)      
 
-	if season.isdigit() and episode.isdigit():
-		season = int(season)
-		episode = int(episode)
+        if season.isdigit() and episode.isdigit():
+                season = int(season)
+                episode = int(episode)
 
-	url = "https://www.addic7ed.com/serie/{}/{}/{}/all".format(name, season, episode)
-	html_dl(url, "allsubs")
+        url = "https://www.addic7ed.com/serie/{}/{}/{}/all".format(name, season, episode)
+        html_dl(url, "allsubs")
 
-	indexall = my_dictionary()
-	get_sub_list(indexall)
-	i = print_allsub(indexall)
-	link,ep = input_sub_choice(indexall, i)
+        indexall = my_dictionary()
+        get_sub_list(indexall)
+        i = print_allsub(indexall)
+        link,ep = input_sub_choice(indexall, i)
 
-	url="https://www.addic7ed.com/{}".format(link)
-	name = name.replace('\'', '')
-	ep = ep.replace('.','_')
-	ver = re.split(r"\-|\ |\+|\_", ep)
-	
-	subname = "{}_S{:02d}E{:02d}_{}.srt".format(name, season, episode, ep).replace(' ','_')
-	sub_dl(url, subname)
+        url="https://www.addic7ed.com/{}".format(link)
+        name = name.replace('\'', '')
+        ep = ep.replace('.','_')
+        ver = re.split(r"\-|\ |\+|\_", ep)
+        
+        subname = "{}_S{:02d}E{:02d}_{}.srt".format(name, season, episode, ep).replace(' ','_')
+        print(subname)
+        sub_dl(url, subname)
 
-	season = "{:02d}".format(season)
-	episode = "{:02d}".format(episode)
-	title = "S{}E{}".format(season, episode)
-	vidname = '{}_S{}E{}_{}.mkv'.format(name, season, episode, ep).replace(' ','_')
+        season = "{:02d}".format(season)
+        episode = "{:02d}".format(episode)
+        title = "S{}E{}".format(season, episode)
+        vidname = '{}_S{}E{}_{}.mkv'.format(name, season, episode, ep).replace(' ','_')
 
-	url = "https://eztv.ro/search/{}".format(name)
-	html_dl(url, "torrentlist")
+        url = "https://eztv.ro/search/{} {}".format(name, title)
+        html_dl(url, "torrentlist")
 
-	torrentdict = get_torrent_list(season, episode, ver)
-	print_torrentlist(torrentdict)
-	torrentchoice = input_torrent_choice(torrentdict)
-	
-	torrentdl(torrentdict, torrentchoice, vidname, title, name)	
-			
-	files = ['torrentlist', 'allsubs', 'tvlist']
-	for file in files:
-		html_remove(file)
+        torrentdict = get_torrent_list(season, episode, ver)
+        print_torrentlist(torrentdict)
+        torrentchoice = input_torrent_choice(torrentdict)
+        
+        torrentdl(torrentdict, torrentchoice, vidname, title, name)     
+                        
+        files = ['torrentlist', 'allsubs', 'tvlist']
+        for file in files:
+                html_remove(file)
