d958151bb732e1313e4cabeeb2a3e866f053b43f
Gregory Arthus <garthus@adminux.fr>
2023-02-25 13:09:22 +0100

    split dl name to handle name with \' like grey\'s anatomy

--- torrentdl.py
+++ torrentdl.py
@@ -7,8 +7,11 @@ def torrentdl(torrentdict, choice, vidname, title, name):
 
     os.environ["TNAME"] = vidname
     os.environ["TLINK"] = list(torrentdict.values())[choice][0]
-    os.environ["SNAME"] = name
+    os.environ["SNAME"] = name.split('\'')[0]
     os.environ["SEP"] = title
+
+    print("title={}".format(title))
+    print("name={}".format(name))
 
 
     os.system('aria2c --seed-time=0 --log-level=info --summary-interval=3600 $TLINK')
